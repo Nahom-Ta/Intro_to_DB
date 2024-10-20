f7a89bf7e8ce25d26921754379d137f7ea1dcb9b
Nahom taye <nahomtayeabdissa@gmail.com>
2024-10-20 11:44:17 +0300

    Add task_4.sql to print full description of the books table

---
+++ task_4.sql
@@ -0,0 +1,5 @@
+-- SQL script to print the full description of the books table
+
+USE alx_book_store;  -- Select the correct database
+
+SHOW COLUMNS FROM books;  -- Print the full description of the books table
