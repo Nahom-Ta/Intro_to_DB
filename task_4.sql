a61cbdbc96a577f49c16a71a498b299a1589a729
Nahom taye <nahomtayeabdissa@gmail.com>
2024-10-20 11:51:01 +0300

    Update task_4.sql to print full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- SQL script to print the full description of the books table
 
-USE alx_book_store;  -- Use the correct database
+USE alx_book_store;  -- Use the correct databasej
 
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';  -- Query to get the details of the books table
+SELECT *
+FROM information_schema.TABLES
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';  -- This retrieves the table information
